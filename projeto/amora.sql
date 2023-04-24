da2706c428c8fe1b7fa63b817aa6ef03f934b97c
Yascsantos <yasmincorreiadossantos290583@gmail.com>
2023-04-24 08:48:40 -0300

    24.04

--- projeto/amora.sql
+++ projeto/amora.sql
@@ -1,93 +1,12 @@
-DROP DATABASE if EXISTS amora; 
-
-CREATE DATABASE amora;
-USE amora;
-
-CREATE table animais(
-    id_animal INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome  VARCHAR(30),
-    historia VARCHAR(1000),
-    idade DECIMAL(10,2),
-    tratamentos VARCHAR(80),
-    doencas VARCHAR(80)
-
-);
-
-CREATE table colaboradores (
-    id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    usuario VARCHAR(50),
-    nome VARCHAR(50),
-    email VARCHAR(40),
-    telefone VARCHAR(20),
-    senha VARCHAR(20),
-    foto VARCHAR(100),
-    cep INT (8),
-    pais VARCHAR(20),
-    estado VARCHAR(5),
-    cidade VARCHAR(50),
-    bairro VARCHAR(20),
-    rua VARCHAR(30),
-    numero INT
-);
-
-CREATE TABLE produto(
-    id_peca INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    imagem VARCHAR(100),
-    preco DECIMAL(10,2),
-    descricao VARCHAR(100)
-);
-
-
-CREATE TABLE adotante(
-    id_adotante INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    id_animal INT,
-    data date,
-
-        FOREIGN KEY (id_user)REFERENCES colaboradores (id_user),
-        FOREIGN KEY (id_animal)REFERENCES animais (id_animal)
-
-);
-
-CREATE TABLE doacao(
-    id_doacao INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    valor DECIMAL(10,2),
-    data date,
-    forma VARCHAR(20), 
-
-    	FOREIGN KEY (id_user)REFERENCES colaboradores (id_user)
-);
-
-CREATE TABLE entrega(
-    id_ent  INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    frete DECIMAL(10,2),
-
-        	FOREIGN KEY (id_user)REFERENCES colaboradores (id_user)
-
-);
-
-CREATE TABLE padrinho(
-    id_padrinho INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    id_animal INT,
-    data date,
-
-        FOREIGN KEY (id_user)REFERENCES colaboradores (id_user),
-        FOREIGN KEY (id_animal)REFERENCES animais (id_animal)
-);
-
-
-
-CREATE TABLE vendas(
-    id_vendas INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    id_user INT,
-    quantidade INT,
-    preco_final DECIMAL(10,2),
-    frete DECIMAL(10,2),
-
-        	FOREIGN KEY (id_user)REFERENCES colaboradores (id_user)
-
-);
-
+UPDATE colaboradores SET 
+				usuario = 'Yas', 
+				nome = 'Yasmin Santos',
+				email = 'yasmin.santos213@etec.sp.gov.br', 
+				telefone = '+5511959819359', 
+				cep = '06528086', 
+				pais = 'Brasil', 
+				estado = 'SP', 
+				cidade = 'Santana de Parnaíba', 
+				rua = 'Astronauta', 
+				numero = '280' 
+	WHERE id_user = '1';
