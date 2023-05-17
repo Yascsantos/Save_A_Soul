98362931c1cdc0c9a1cc3617796230a451d443d8
daicastr01 <daiane.castro01@etec.sp.gov.br>
2023-05-17 10:49:46 -0300

    17.05-atualizacao

--- projeto/index/index.php
+++ projeto/index/index.php
@@ -37,17 +37,14 @@
         </div>
 
         <div class="sobre">
-          <br>
-            <h2>Como Ajudar</h2><br>
-            
-            <p> Você pode nos ajudar tanto comprando produtos da nossa loja, onde o dinheiro da compra vai ser usado nos resgates,  <br>
-            quanto fazendo uma doação, apadrinhamento ou adotando um dos nossos anjinhos.</p>
+          
+          <img scr="./imgs/linha.png">
 
         </div>
           
 
         <div class="vitimas">
-            <img scr="../imgs/dog2.jpg">
+            <img scr="./imgs/dog2.jpg">
             <h2>Vítimas que só aumentam</h2>
 
           <p class="text"><i>São Paulo contabiliza aumento de 15,6% nas denúncias de maus-tratosa em 2021.</i> </p>
