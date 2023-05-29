80fb67f1905770ac071f2e6690e70bf5eb988c06
daicastro01 <daiane.castro01@etec.sp.gov.br>
2023-05-28 21:17:31 -0300

    28-05

--- projeto/usuario/perfil/perfil.php
+++ projeto/usuario/perfil/perfil.php
@@ -35,89 +35,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/perfil.css" type="text/css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    function recarregarParte() {
+      location.reload();
+    }
+    function exibirIframe() {
+      var iframeContainer = document.getElementById("iframeContainer");
+      iframeContainer.style.display = "block";
+      iframeContainer.innerHTML = '<iframe src="endereco.php"></iframe>';
+    }
+  </script>
     <title>perfil</title>
     <style>
-    .popup-overlay {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 50%;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: 9999;
-      opacity: 0;
-      pointer-events: none;
-      transition: opacity 0.3s ease;
-    }
-
-    .popup-content {
-      background-color: purple;
-      padding: 20px;
-      border-radius: 5px;
-      max-width: 400px;
-      text-align: center;
-    }
-
-
-    .close-button {
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      cursor: pointer;
-    }
-    .iframe-container {
+    <style>
+    #iframeContainer {
       display: none;
     }
 
-    .iframe-container.show {
-      display: block;
-    }
-    .input-img{
-    width: 50%;
-    height: 15px;
-    background-color: rgba(255, 255, 255, 0.32);
-    border-radius: 20px;
-    outline: none;
-    border: 2px solid transparent;
-    color: #616161;
-    transition: all 0.4s ease;
-    }
- input button{
-    width: 50%;
-    height: 47px;
-    background:  hsl(240, 28%, 25%);
-    border-radius: 20px;
-    outline: none;
-    border: none;
-    margin-top: 15px;
-    color: white;
-   margin-left: 20%;
-    font-size: 16px;
-}
-
   </style>
-  <script>
-    function showIframe() {
-      var iframeContainer = document.querySelector('.iframe-container');
-      iframeContainer.classList.add('show');
-    }
-  </script>
-  <script>
-    function openPopup() {
-      var popupOverlay = document.querySelector('.popup-overlay');
-      popupOverlay.style.opacity = '1';
-      popupOverlay.style.pointerEvents = 'auto';
-    }
 
-    function closePopup() {
-      var popupOverlay = document.querySelector('.popup-overlay');
-      popupOverlay.style.opacity = '0';
-      popupOverlay.style.pointerEvents = 'none';
-    }
-  </script>
 </head>
 
 
@@ -147,14 +85,13 @@
 
 </div>
 
-    <form class='perfil'>
     
+<form class='perfil'>
     <p>Seja bem vinda ao seu perfil <strong><?php foreach ($instrucao as $exibe){ echo $exibe['usuario'];}?></strong></p><br>
-                    <!--Colocando a imagem padrão do perfil -->
-                    <div> </div>
-	<!--Dados pessoais-->
-        <div class="dados">
-        <?php
+                  
+        <!--Colocando a imagem padrão do perfil -->
+  <?php
+  
                         $sql_query = "select * from $tabela where $campo ='$id'";
                         // executando instrução SQL
                         $requery = mysqli_query($conexao,$sql_query);
@@ -166,23 +103,25 @@
                         }
                     
                ?>
-           <button onclick="openPopup()">Editar imagem</button>
 
-        <div class="popup-overlay">
-        <div class="popup-content">
-        <form action="img.php" method="POST" enctype="multipart/form-data">
-                    <label>Título: </label><br />
-                    <input type="text" name="titulo" required class="input-img"/><br />
-                     <label>Selecione o arquivo:</label>
-                    <input type="file" name="arquivo" size="45" class="submit"/></label> <br />
-                    <button type="submit" value="Enviar" name="enviar" class="submit">Enviar</button>
-                    <button type="reset" value="Apagar" class="submit">Apagar</button>
-                </form>
-            <span class="close-button" onclick="closePopup()">Fechar</span>
-        </div>
-        </div>
+      <!-- Botão para abrir o popup -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Editar imagem</button>
 
+<!-- Popup -->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
 
+      <!-- Conteúdo do popup -->
+      <div class="modal-body">
+        <iframe src="img.php" width="560" height="100px" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+        <button type="button"  onclick="recarregarParte()">Atualizar</button>
+    </div>
+  </div>
+</div>
+
+<div>
           <b>  <p>Usuário:  <?php foreach ($instrucao as $exibe){ echo $exibe['usuario'];}?><br>
 		
 
@@ -195,14 +134,15 @@
 
        Senha:  <?php foreach ($instrucao as $exibe){ echo $exibe['senha'];}?></p></b> 
        <br>
-       <button onclick="showIframe()">Exibir Endereço</button>
-
-<div class="iframe-container">
-  <iframe src="endereco.php" width="600" height="200" frameborder="0"></iframe> 
+       <button onclick="exibirIframe()">Exibir endereco</button>
+  <div id="iframeContainer"></div>
  
-</div> <a href='./updates/form.php' class="submit"><button type='button'>Editar</button></a>
+       </div>
+    
+
+<a href='./updates/form.php' class="submit"><button type='button'>Editar</button></a>
            
-      <br><br>
+    
   
 
        </form>
