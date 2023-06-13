e07979142656d2f9b983b4a7bfe0bf46d7aa4b9f
Yascsantos <yasmincorreiadossantos290583@gmail.com>
2023-06-13 17:40:19 -0300

    13.06

---
+++ projeto/adm/produtos/categoria/update/alter.php
@@ -0,0 +1,62 @@
+<?php
+	include_once("../../../../conexaoBD.php");
+	$tabela="categoria";
+	$id = "id_cat"; 
+    $campo="nome";
+	$edit = $_GET['codigo'];
+		
+    $sqlPesq = "SELECT $campo FROM $tabela WHERE $id= $edit";
+	$pesquisa = mysqli_query($conexao,$sqlPesq);
+	
+	if (!$pesquisa)
+	{
+		die(' Query Inválida: ' . mysqli_error($conexao));
+	} 
+
+    else 
+    {
+        echo "Deu certo";
+    }
+	
+	if (isset($_POST['alterar'])) {
+		$codigo = $_POST['codigo'];
+		$nome = $_POST['nome'];
+		
+		$sql = "UPDATE $tabela SET 
+			nome = '$nome',
+			WHERE $id = $codigo";
+			
+		$instrucao = mysqli_query($conexao,$sql);
+		
+		if (!$instrucao) {
+			die(' Query Inválida: ' . mysqli_error($conexao));
+	
+		} 
+        else 
+        {
+			mysqli_close($conexao);
+            echo "Mudança realidaza com sucesso";
+			exit;
+		}
+	}	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=c">
+    <title>Alteração da categoria</title>
+</head>
+<body>
+    <h1>Cadastro de categorias</h1>
+    <form action="" method="post">
+        <input type="hidden" name="codigo" value="<?= $edit;?>" />
+        <label>Categoria: </label> <br>
+        <input type="text" placeholder="Mude a categoria" name="nome">
+        <input type="submit" name="alterar" value="Alterar categoria">
+    </form>
+
+
+</body>
+</html>
