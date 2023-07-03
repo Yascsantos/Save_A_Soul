a822b84e7e6cc0313321ca17dd95cdea2f818655
Yascsantos <yasmincorreiadossantos290583@gmail.com>
2023-07-03 18:05:10 -0300

    03.07

--- projeto/usuario/perfil/img.php
+++ projeto/usuario/perfil/img.php
@@ -12,13 +12,7 @@
 	$id_user = $_SESSION['id_user'];
 
 
-	/*
-		$ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
-		$new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-		$dir = './imagens/'; //Diretório para uploads 
-		move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
-		echo("Imagen enviada com sucesso!");
-	*/
+
 	if(isset($_POST['Enviar']))
 	{
 		if(isset($_FILES['pic']))
