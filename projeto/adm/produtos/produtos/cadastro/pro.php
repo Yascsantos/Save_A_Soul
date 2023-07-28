9cc1aeaff2cbd7600f9a2b2ce7ea40e4d5e829c1
daicastro01 <daiane.castro01@etec.sp.gov.br>
2023-07-28 20:07:21 -0300

    28/07

--- projeto/adm/produtos/produtos/cadastro/pro.php
+++ projeto/adm/produtos/produtos/cadastro/pro.php
@@ -6,14 +6,133 @@
 
     <title>Cadastrar informações</title>
 </head>
-<body>
-    <h2> Cadastrar mais informações dos produtos</h2>
-    <div>
-    <a href="../cadastro/form.php"> Cadastrar + produtos </a><br>
-    <a href="../pg02.html"> Voltar</a> </div>
-    <br> <br>
-    <p>Clique na imagem para cadastrar, mudar ou deletar a imagem padrão do produto</p>
+ <Style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+        body{
+        font-family: 'Poppins';
+        background-size:1500px 900px;
+        background-image:url(../../../../petshop/imgs/fundo1.jpg);
 
+        color:white ;
+       
+    }
+       
+  #menu{
+  position: fixed;
+  width:100%;
+  height:110px;
+  font-family: 'Poppins';
+
+ 
+}
+img{
+  width:100px;
+  height:100px;
+margin-top:auto;
+}
+
+  nav {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-family: 'Poppins';
+    height: 12vh;
+  }
+  
+
+  .nav-list {
+    list-style: none;
+    display: flex;
+  
+  }
+  
+  .nav-list li {
+    letter-spacing: 3px;
+    margin-left: 32px;
+    margin-top: auto;
+   
+  }
+  section{
+    padding: 55px 0;
+  }
+  section .title{
+    color: #fff;
+    font-family: 'poppins';
+    text-align: center;
+    font-size: 40px;
+    font-weight: 500;
+    margin-bottom: 60px;
+    padding-bottom: 20px;
+    position: relative;
+  }
+  section .title::before{
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 180px;
+    height: 3px;
+    background: #ffffff;
+    transform: translateX(-50%);
+  }
+  section .title::after{
+    COLOR: #ffffff;
+    position: absolute;
+    bottom:-12px;
+    left: 50%;
+    font-size: 20px;
+    background:  #d4987e;
+    color: #f1e8ec;
+    padding: 5px;
+    border-radius: 10%;
+    transform: translateX(-50%);
+  }
+
+
+.max-width{
+    max-width: 1300px;
+    padding: 0 80px;
+    margin: auto;
+}
+#menu a{
+    font-family: 'Poppins';
+    font-size: 0.9rem;
+    text-decoration:none;
+    font-size: 0.9rem;
+    font-weight:600;
+    color:#fcfcfc;
+  }
+ 
+  #menu a:hover{
+    color:#3a3a3b;
+  }
+  .msg{
+    background-color: #3a3a3b;
+    width: 300px;
+    height: 130px;
+    text-align: center;
+    border-radius: 3px;
+  }
+    </style>
+</head>
+<body>
+<header id="menu">
+      <nav>
+   <img src="../../../../index/imgs/logo.png" >
+     <ul class="nav-list">
+          <li> <a href="../cadastro/form.php">CADASTRAR + ANIMAIS</a></li>
+          <li> <a href="../pg02.html">VOLTAR</a></li>
+          <br>
+       </ul>
+
+       </nav>
+</header> 
+
+<section class="sobre" id="sobre">
+        <div class="max-width">
+    <h2 class='title'> Cadastrar mais informações dos produtos</h2>
+    <div class='msg'><br>Clique na imagem para cadastrar, mudar ou deletar a imagem padrão do produto<br><br></div>
+</div></section>
 <?php
     include_once("../../../../conexaoBD.php");
     $sql= "SELECT  id_pro AS cod, nome_cat AS categoria, prod AS produto, preco, qtd AS quantidade, modelo, outros,img_pro 
@@ -25,28 +144,20 @@
 
     echo "
     <style>
-    body{
-        background-size:1500px 900px;
-        background-image:url(../../../../index/imgs/fundo1.jpg);
-        background-position: cr ;
-        background-repeat: no-repeat;
-        color:white ;
-       
+    * {
+    box-sizing: border-box;
+    font-family: 'Poppins';
     }
+ 
        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
 
-    * {
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-        }
-     
+ 
     
      a{
      font-family: 'Poppins', sans-serif;
-     font-size: 0.9rem;
-     font-size: 0.9rem;
-     font-weight:600;
+     font-size: 12px;
      color:#333235;
+     text-decoration:none;
      }
      
      
@@ -55,7 +166,7 @@
      }
      
      table, th, td {
-     padding: 8px;
+     padding: 4px;
      width: 50%;
      top:auto;
   
