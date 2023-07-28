9969d66aa89b2cde809bf88f227475c03f09ac7f
Yascsantos <yasmincorreiadossantos290583@gmail.com>
2023-07-28 13:57:10 -0300

    28.07

---
+++ projeto/petshop/proc.php
@@ -0,0 +1,32 @@
+<?php
+/*
+    Fazer a soma geral de todos os valores do carrinho do usuário que está com o id dentro da sessão que está
+    iniciada
+*/
+    include_once("../conexaoBD.php");
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    
+    $id = $_SESSION['id_p'];
+    $id_user = $_SESSION['id_user']; 
+
+    $sql="SELECT valor FROM carrinho WHERE id_user= $id_user";
+    $instrucao= $conexao->query($sql) or die("Falha na execução do códigdo SQL: ". mysqli_error($conexao));
+
+    $teste = 0;
+
+    foreach($instrucao as $dados)
+    {
+        $valor = $dados['valor'];
+        $teste += $valor;
+    }
+
+    echo $teste;
+
+
+
+
+    
+?>
