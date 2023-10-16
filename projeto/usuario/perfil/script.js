4381c911f5898baf15e7a27f96bad406430e137f
daicastr01 <daiane.castro01@etec.sp.gov.br>
2023-10-16 12:04:37 -0300

    16/10

--- projeto/usuario/perfil/script.js
+++ projeto/usuario/perfil/script.js
@@ -1,5 +1,33 @@
 
+const cartModal = document.getElementById('cart-modal');
+const openCartButton = document.getElementById('open-cart');
+const closeCartButton = document.getElementById('close-cart');
 
+// Event listener to open the cart modal
+openCartButton.addEventListener('click', () => {
+    cartModal.style.display = 'block';
+});
+
+// Event listener to close the cart modal
+closeCartButton.addEventListener('click', () => {
+    cartModal.style.display = 'none';
+});
+
+function resizeIframe() {
+    var iframe = document.getElementById("myIframe");
+    var body = iframe.contentDocument.body,
+        html = iframe.contentDocument.documentElement;
+
+    var height = Math.max(
+        body.scrollHeight, body.offsetHeight,
+        html.clientHeight, html.scrollHeight, html.offsetHeight
+    );
+
+    iframe.style.height = height + "px";
+}
+
+// Chama a função quando o conteúdo dentro do iframe é carregado
+document.getElementById("myIframe").onload = resizeIframe;
 function resizeIframe() {
     var iframe = document.getElementById("myIframe");
     var body = iframe.contentDocument.body,
