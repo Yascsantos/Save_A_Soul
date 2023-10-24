f7b2fc634affc38cd97017c9ecaf9479f158eb71
Yascsantos <yasmincorreiadossantos290583@gmail.com>
2023-10-24 20:16:37 -0300

    24/10

--- projeto/testes/teste.php
+++ projeto/testes/teste.php
@@ -7,19 +7,18 @@ require_once ('../vendor/autoload.php');
 
 $mail = new PHPMailer(true);
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->SMTPSecure = 'tls';
-    $mail->Username   = 'yasminsantosyasxp@gmail.com';          //SMTP username
-    $mail->Password   = 'Ycds301106';                           //SMTP password
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+// Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Use SSL (change from 'tls' to 'ssl')
+    $mail->Username   = 'saveasoul2023@gmail.com';              // SMTP username
+    $mail->Password   = 'savesoul';                             // SMTP password
+    $mail->Port       = 465;                                    // TCP port to connect to (use 465 for SSL)
 
-
-    $mail->setFrom('yasminsantosyasxp@gmail.com', 'Destinatario');
-    $mail->addAddress('yaminsantos.21174@gmail.com', 'Remetente');     
+    $mail->setFrom('saveasoul2023@gmail.com', 'Destinatario');
+    $mail->addAddress('saveasoul2023@gmail.com', 'Remetente');     
 
 
     $mail->isHTML(true);                                
