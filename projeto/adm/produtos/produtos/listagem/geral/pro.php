3b93a2c891220de9d9509bd482abb5dfdcc58d4a
daicastr01 <daiane.castro01@etec.sp.gov.br>
2023-11-10 11:37:44 -0300

    10/11

--- projeto/adm/produtos/produtos/listagem/geral/pro.php
+++ projeto/adm/produtos/produtos/listagem/geral/pro.php
@@ -5,59 +5,85 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Grade de produtos</title>
 </head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+    main{
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  min-height: 10vh;
+
+}
+a {
+    
+      font-size: 0.9rem;
+      text-decoration:none;
+      font-size: 0.9rem;
+      font-weight:600;
+      color: black;
+      
+    }
+   
+    a:hover{
+      color:#3a3a3b;
+    }
+    .encontro{
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+    width: 700px;
+     margin-top: 200px;
+    font-family: 'Poppins', sans-serif;
+    background-color: rgba(255, 255, 255, 0.993);
+    padding: 30px 40px;
+    text-align: justify;
+    }
+.img{
+    display: flex;
+}
+body{
+        font-family: 'Poppins';
+        background-size:cover;
+        background-image:url(../../../../index/imgs/fundo.jpg);
+
+       
+    }
+
+</style>
 <body>
+    <main>
+    <div class='encontro' > 
+
     <h1>Listagem dos produtos cadastrados</h1>
     <a href="../../pg02.html">Voltar</a>
-</body>
-</html>
+
 <?php
      include_once("../../../../../conexaoBD.php");
      $sql_code= "SELECT * FROM produto";
      $pesquisa = mysqli_query($conexao,$sql_code);
                 
                 
-     echo "<table>";
-     echo "                        
-         <tr>
-             <th align='center'></th>
-             <th> </th>
-         </tr>
-                
-         <tr>
-             <th align='center'></th>
-             <th></th>
-         </tr>
-                
-         <tr></tr>
-         <tr></tr>
-         ";
+     echo "<div class='img'>";
+  
                 
      foreach ($pesquisa as $exibe) 
      {
          echo "
-            <tr>
-                <td align='center'>------------------------------</td>
-                <td></td>
-            </tr> 
+            
         
-             <tr>
-                 <td align='center'><a href='list.php?&codigo=".$exibe['id_pro']."'><img src=".$exibe['img_pro']." alt='Imagem padrão' width='100px' height='100px'></a></td>
-                 <td></td>
-             </tr>
+           
+                 <div align='center'> <a href='list.php?&codigo=".$exibe['id_pro']."'><img src=".$exibe['img_pro']." alt='Imagem padrão' width='100px' height='100px'></a>
                 
-             <tr>
-                 <td align='center'><a href='list.php?&codigo=".$exibe['id_pro']."'>".$exibe['prod']."</td>
-                 <td></td>
-             </tr> 
-                
-             <tr></tr><tr></tr>
+           
+                 <div align='center'><a href='list.php?&codigo=".$exibe['id_pro']."'>".$exibe['prod']."</div>
+          
              ";
                 
      }
-     echo "<tr>
-                <td align='center'>------------------------------</td>
-                <td></td>
-            </tr>";
-     echo"</table>";
+    
+     echo"</div>";
 
 ?>
+</div>
+</body>
+</html>
