ecd0a117f75842298d4a676fbfe35e813103277c
Yascsantos <yasminsantos.21174@gmail.com>
2023-11-24 11:39:11 -0300

    24/11

--- projeto/imagens_pro/alter.php
+++ projeto/imagens_pro/alter.php
@@ -65,7 +65,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../usuario/perfil/css/perfil.css" type="text/css">
+	
 	<title>Update de imagem</title>
 </head>
 <style>
