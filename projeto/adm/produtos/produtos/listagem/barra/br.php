3132f3b368afd6ffadd9eac90a17fe852341bdf5
daicastro01 <daiane.castro01@etec.sp.gov.br>
2023-11-29 20:55:56 -0300

    29/11

--- projeto/adm/produtos/produtos/listagem/barra/br.php
+++ projeto/adm/produtos/produtos/listagem/barra/br.php
@@ -1,37 +1,18 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Petshop| Save a Soul</title>
-    <link rel="icon" type="imagem/png" href="../index/imgs/logo1.png" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.cdnfonts.com/css/lazy-dog" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="../../../../../petshop/css/petshop.css" type="text/css"></head>
+    <link rel="stylesheet" href="../pet/style.css">
+    <title>Petshop|Save a soul</title>
 </head>
-<style>
-   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+<style>   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
 
 * {
   font-family: 'Poppins';
 }
 
-
-button {
-  width: 30%;
-  height: 30px;
-  background: #adb5a0;
-  
-  outline: none;
-  border: none;
-  margin-top: 15px;
-  color: white;
-  margin-left: 20%;
-  font-size: 16px;
-}
 
 
 
@@ -49,8 +30,6 @@ button {
   background-color: #fff;
   border: 1px solid #ccc;
   position: absolute;
-  top: 40%;
-  left: 20%;
   transform: translate(-50%, -50%);
   text-align: center;
 }
@@ -113,24 +92,46 @@ button {
     font-size: 13px;
  
 
-  }
-  
-</style>
+  }</style></style>
 <body>
-<header id="menu">
-      <nav>
-   <img src="../../../../../index/imgs/logo.png" >
-      
-     <ul class="nav-list">
-     <li> <a href="../../../../../index/ajude.php">COMO AJUDAR?</a></li>
-</ul>
-       <div class='filt'><?php  include_once("../barra/barra.php"); ?>  
-      
 
-      <butonn class='lupa'><span class='material-symbols-outlined' onclick='openPopup()'>
-     tune
-     </span></butonn>
-     <div id='popupContainer' style='display: none;'>
+   
+        <header>
+            <div class="logo">
+         
+            <img src="../../../../../index/imgs/logo.png" >
+     
+            </div><!--logo-->
+
+            <div class="cabeçalho-link">
+                <li>
+                   <a href="../../../../../index.php">Inicio</a>
+                </li>
+            
+                <li> <a href="../../../../../index/ajude.php">Como ajudar?</a></li>
+
+<?php  include_once("../barra/barra.php"); ?>  
+      
+            </div><!--cabeçalho-link-->
+            <div class="icon"onclick='openPopup()'><span><ion-icon name="filter"></ion-icon></span></div>
+            <div class="icon" id="open-cart"><span><ion-icon name="bag-outline"></ion-icon></span></div>
+            <div id="cart-modal" class="carrinho">
+
+<div class="content-carrinho">
+<span class="fechar" id="close-cart">&times;</span>
+    Carrinho de Compras
+ 
+    <iframe src="exibe_car.php" id="car"></iframe>
+  
+
+  <a href='grade_nova.php'>Continuar comprando</a>    <button  class='comprar'>    <a href='../../../../../petshop/obtencao/forma.php'><b>Finalizar compra</b></button></a>
+   </div>
+</div>
+
+<script src="script.js"></script>
+         
+            <div class="icon" ><a href="../../../../../usuario/perfil/perfil.php"><span><ion-icon name="person"></a></ion-icon></span></div>
+            <div id='popupContainer' style='display: none;'>
                            <div  class='popup' >
              
                            <br><br>
@@ -146,47 +147,15 @@ button {
                  
                        </script>
                      </div> 
-                     <ul class="nav-list">
-                     <li> <a href="../../../../../index.php"><span class="material-symbols-outlined">
-home
-</span></a></li>
+
+        </header> 
+        <BR>
     
-          <li><a id="open-cart"><span class="material-symbols-outlined">
-shopping_cart
-</span></a> </li>
-          <div id="cart-modal" class="carrinho">
-
-<div class="content-carrinho">
-<span class="fechar" id="close-cart">&times;</span>
-    Carrinho de Compras
- 
-    <iframe src="../pet/exibe_car.php" id="car"></iframe>
-  
-
-  <a href='../pet/grade_nova.php'>Continuar comprando</a>    <button  class='comprar'>    <a href='../../../../../petshop/obtencao/forma.php'><b>Finalizar compra</b></button></a>
-   </div>
-</div>
-
-<script src="../pet/script.js"></script>
-          <li><a href="../../../../../usuario/perfil/perfil.php"><span class="material-symbols-outlined">account_circle</span></a></li>
-          <br>
-       </ul>
-       </nav>
-</header>
-<section class="filtro">
-<a href='#produto' class='but'></a>
-
-                        </section>
-     
-  
-        <section  id="produto">
-
-      <h2 class='title'>Tudo para seu pet</h2>
-       
-      <div class="filtro-content">
+    <section>
+        <h1>PRODUTOS ENCONTRADOS</h1>
+        <div class="Container-produto">
       
-               
-                       
+              
     <?php
             if(isset($_POST['pesquisar']))
             {
@@ -205,18 +174,14 @@ shopping_cart
                     echo "
                 
                       
-                    <div class='card'>
- 
-                    <a href='../../../../../petshop/perfil.php?&codigo=".$exibe['id_pro']."'><img src=".$exibe['img_pro']." style='height:250px;  width:250px;'></a><br>
-                        
-                         <b>   ".$exibe['prod']."</b><br> <p> R$".$exibe['preco']."</p><br>
-                         <div class='compra'>
-                         <a href='../../../../../petshop/perfil.php?&codigo=".$exibe['id_pro']."'> <span class='material-symbols-outlined' >visibility</span></a>
-                         <a href='../../../../../petshop/perfil.php?&codigo=".$exibe['id_pro']."'><span class='material-symbols-outlined' id='ver'>
-                         shopping_cart
-                         </span></a>
-                            </div>
-                              </div>";
+                  
+            <div class='produto'>
+            <a href='../../../../../petshop/perfil.php?&codigo=".$exibe['id_pro']."'>  <img src=".$exibe['img_pro']." style='height:250px;  width:250px;'>
+            </a>
+          <p> ".$exibe['prod']."</p>
+                <h5>R$".$exibe['preco']."</h5>
+                  <ion-icon name='cart-outline'></ion-icon>
+            </div>";
                 }
               
               //Campo nulo 
@@ -232,10 +197,121 @@ shopping_cart
     
 ?>
 
+
 </div>
+
     </section>
+    <section>
+        <div class="inicio">
+            <br>
+            <h1>PARTICIPE ATIVAMENTE </h1>
+            <H1>COMPRANDO NOSSOS </H1><P>PRODUTOS PARTE DO LUCRO DAS VENDAS VÃO PARA MANUTENÇÃO DA ONG</P>
+            <A HREF='../../../../../index/ajude.php'> <button>ver mais</A></button>
+          
+
+        </div>
+    </div> 
+         
+</section>
+
+
+<section>
+        <h1>VARIEDADE DE PRODUTOS</h1>
+        <div class="tipos">
+        <div class="cards">
+                <img src="../../../../../petshop/imgs/ali.png">
+            </div>
+            <div class="cards">
+                <img src="../../../../../petshop/imgs/ca.jpg">
+            </div>
+            <div class="cards">
+                <img src="../../../../../petshop/imgs/grande.jpg">
+            </div>
+</div>
+    
+    </section>
+
+    <section class="promo">
+        <div class="text-promo">
+            <h6> NA PRIMEIRA COMPRA</h6>
+           
+            <h4>20% OFF<br>CONFIRA JÁ</h4>
+            <a href="#" class="btn">Compre agora</a>
+        
+        </div>
+    </section><!--cta-->
+
+    <section class="contato" id='contato'>
+        <div class="max-width">
+            <h2>Contato</h2>
+            <div class="contato-content">
+               <div class="column left">
+
+                   <p>Fazemos valer as leis que protegem os animais, fiscalizamos os órgãos públicos e denunciamos crimes. Isso é dar voz a quem não pode falar, e quanto mais gente, melhor! Por isso contamos com você nessa tarefa!</p>
+               
+               <div class="icons">
+                   <div class="row">
+                        <ion-icon name="person-outline"></ion-icon>
+                        <div class="info">
+                            <div class="head">Nome</div>
+                            <div class="sub-title">Ong-Save a Soul</div>
+                        </div>
+                   </div>
+                   <div class="row">
+                    <ion-icon name="earth-outline"></ion-icon>
+                    <div class="info">
+                        <div class="head">Endereço</div>
+                        <div class="sub-title">Endereço: Av. Ten. Marques, s/n - Fazendinha, Santana de Parnaíba - SP, 06529-001</div>
+                    </div>
+               </div>
+               <div class="row">
+                <ion-icon name="person-outline"></ion-icon>
+                <div class="info">
+                    <div class="head">Email</div>
+                    <div class="sub-title">saveasoul2023@gmail.com</div>
+                </div>
+           </div>
+               </div>
+            </div><!--column left-->
+            <div class="column rigth">
+                <div class="text">Mensagem</div>
+                <form action="#">
+                    <div class="fields">
+                        <div class="field name">
+                            <input type="text" placeholder="Nome" required>
+                        </div>
+                        <div class="field email">
+                            <input type="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="field">
+                            <input type="text" placeholder="Sobrenome" required>
+                        </div>
+                        <div class="field textarea">
+                           <textarea  cols="30" rows="10" placeholder="Escrever...." required></textarea>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!--sessão  footer-->
+    <footer>
+        <span>Criado para <a href="#"></a>Fins educacionais | TCC dos discentes da Etec-Bartolomeu Bueno da Silva 2023</span>
+    </footer>
+
+                
+
+    
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="js/script.js"></script>
 </body>
-</html>
-<script src="../../../../../petshop/js/script.js"></script>
-
-
+</html> 
