6e0bb131132ecb702860d3b8e7c1424ed62030bb
daicastr01 <daiane.castro01@etec.sp.gov.br>
2023-12-06 11:33:48 -0300

    06/12

--- projeto/dados/dados_dai.sql
+++ projeto/dados/dados_dai.sql
@@ -1,43 +1,53 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.7.33 - MySQL Community Server (GPL)
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.2.0.6213
+-- HeidiSQL Versão:              12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Copiando dados para a tabela save.adocao: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `adocao` DISABLE KEYS */;
-REPLACE INTO `adocao` (`id_ado`, `id_user`, `id_ani`, `data_entre`, `horario`, `status`) VALUES
+-- Copiando dados para a tabela save.adocao: ~1 rows (aproximadamente)
+INSERT INTO `adocao` (`id_ado`, `id_user`, `id_ani`, `data_entre`, `horario`, `status`) VALUES
 	(1, 2, 2, '2023-11-11', '14:11:00', 'Aprovado');
-/*!40000 ALTER TABLE `adocao` ENABLE KEYS */;
+
+-- Copiando dados para a tabela save.animal: ~0 rows (aproximadamente)
+INSERT INTO `animal` (`id_ani`, `nome_ani`, `raca`, `porte`, `sexo`, `img_ani`, `status`, `modalidade`) VALUES
+	(1, 'Billy', 'Vira Lata', 'Médio', 'macho', '../listagem/img/2023.12.05-21.41.27webp', 'disponivel', '---'),
+	(2, 'Mel', 'Vira Lata', 'Grande', 'femea', '../listagem/img/2023.12.05-22.03.30jfif', 'disponivel', '---'),
+	(3, 'Zorro', 'Vira Lata', 'Médio', 'macho', '../listagem/img/2023.12.05-22.05.04webp', 'disponivel', '---'),
+	(4, 'Boro', 'Salsicha', '', 'Macho', '../listagem/img/2023.12.05-22.07.03jfif', 'disponivel', '---'),
+	(5, 'Lola', 'Pincher', 'Grande', 'femea', '../listagem/img/2023.12.05-22.13.08.jpg', 'disponivel', '---'),
+	(6, 'Félix', 'Bombaim', 'Pequeno', 'macho', '../listagem/img/2023.12.05-22.30.54jfif', 'disponivel', '---'),
+	(7, 'Pérola', 'Bombaim', 'Médio', 'femea', '../listagem/img/2023.12.05-22.32.05jfif', 'disponivel', '---'),
+	(9, 'Max', 'Korat ', 'Grande', 'macho', '../listagem/img/2023.12.05-22.33.53webp', 'disponivel', '---'),
+	(10, 'Pelly', 'Vira Lata', 'Médio', 'femea', '../listagem/img/2023.12.05-22.35.08jfif', 'disponivel', '---'),
+	(11, 'Luffi', 'Turkish Van', 'Pequeno', 'macho', '../listagem/img/2023.12.05-22.37.17.jpg', 'disponivel', '---'),
+	(12, 'Percy', 'Siames', 'Médio', 'macho', '../listagem/img/2023.12.05-22.38.37jfif', 'disponivel', '---'),
+	(13, 'Petúnia', 'Persa', 'Grande', 'femea', '../listagem/img/2023.12.05-22.39.43.jpg', 'disponivel', '---');
 
 -- Copiando dados para a tabela save.armazena: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `armazena` DISABLE KEYS */;
-REPLACE INTO `armazena` (`id_arm`, `id_user`, `id_pro`, `numero`, `cor`, `tamanho`, `qtd`, `valor`, `data`) VALUES
+INSERT INTO `armazena` (`id_arm`, `id_user`, `id_pro`, `numero`, `cor`, `tamanho`, `qtd`, `valor`, `data`) VALUES
 	(1, 2, 2, 14, 'Azul', '15cm', 60, 1200.00, '2003-11-23'),
 	(3, 2, 2, 14, 'Marrom', '15cm', 12, 240.00, '2003-11-23');
-/*!40000 ALTER TABLE `armazena` ENABLE KEYS */;
 
--- Copiando dados para a tabela save.carrinho: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `carrinho` DISABLE KEYS */;
-REPLACE INTO `carrinho` (`id_car`, `id_user`, `id_pro`, `cor`, `tamanho`, `qtd`, `valor`) VALUES
+-- Copiando dados para a tabela save.carrinho: ~5 rows (aproximadamente)
+INSERT INTO `carrinho` (`id_car`, `id_user`, `id_pro`, `cor`, `tamanho`, `qtd`, `valor`) VALUES
 	(1, 1, 2, 'Roxo', '15cm', 30, 600.00),
 	(3, 1, 1, 'Roxo', 'M', 10, 150.00),
 	(11, 3, 1, 'Roxo', 'M', 3, 45.00),
 	(12, 3, 1, 'Roxo', 'M', 3, 45.00),
 	(13, 3, 2, 'Roxo', '20cm', 9, 180.00);
-/*!40000 ALTER TABLE `carrinho` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.categoria: ~8 rows (aproximadamente)
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-REPLACE INTO `categoria` (`id_cat`, `nome_cat`) VALUES
+INSERT INTO `categoria` (`id_cat`, `nome_cat`) VALUES
 	(1, 'Roupas'),
 	(3, 'Sapatos'),
 	(4, 'Acessorios'),
@@ -47,11 +57,8 @@ REPLACE INTO `categoria` (`id_cat`, `nome_cat`) VALUES
 	(8, 'Higiene'),
 	(9, 'Potes');
 
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-
 -- Copiando dados para a tabela save.cores: ~24 rows (aproximadamente)
-/*!40000 ALTER TABLE `cores` DISABLE KEYS */;
-REPLACE INTO `cores` (`id_cor`, `id_pro`, `cor`) VALUES
+INSERT INTO `cores` (`id_cor`, `id_pro`, `cor`) VALUES
 	(1, 1, 'Roxo'),
 	(2, 1, 'Azul'),
 	(5, 2, 'Marrom e cinza'),
@@ -75,27 +82,25 @@ REPLACE INTO `cores` (`id_cor`, `id_pro`, `cor`) VALUES
 	(23, 13, 'Branco'),
 	(25, 16, 'estampa de coracao'),
 	(26, 17, 'Multicolorido Collar'),
-	(27, 18, 'Bege');
-/*!40000 ALTER TABLE `cores` ENABLE KEYS */;
+	(27, 18, 'Bege'),
+	(28, 19, 'rosa '),
+	(29, 19, 'amarelo'),
+	(30, 19, ' vermelho '),
+	(31, 19, 'azul');
 
 -- Copiando dados para a tabela save.doacao: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `doacao` DISABLE KEYS */;
-REPLACE INTO `doacao` (`id_doa`, `id_par`, `data`, `forma`) VALUES
+INSERT INTO `doacao` (`id_doa`, `id_par`, `data`, `forma`) VALUES
 	(1, 1, '2023-11-03', 'Dinheiro'),
 	(2, 2, '2023-11-03', 'Mantimentos');
-/*!40000 ALTER TABLE `doacao` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.imgani: ~3 rows (aproximadamente)
-/*!40000 ALTER TABLE `imgani` DISABLE KEYS */;
-REPLACE INTO `imgani` (`id_imgani`, `id_ani`, `img`) VALUES
+INSERT INTO `imgani` (`id_imgani`, `id_ani`, `img`) VALUES
 	(2, 11, '../img_ani/2023.11.03-17.51.03jfif'),
 	(3, 2, '../img_ani/2023.11.03-17.52.03jfif'),
 	(4, 12, '../img_ani/2023.11.03-17.52.28jfif');
-/*!40000 ALTER TABLE `imgani` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.imgpro: ~14 rows (aproximadamente)
-/*!40000 ALTER TABLE `imgpro` DISABLE KEYS */;
-REPLACE INTO `imgpro` (`id_imgpro`, `id_pro`, `img`) VALUES
+INSERT INTO `imgpro` (`id_imgpro`, `id_pro`, `img`) VALUES
 	(7, 1, '../img_pro/2023.12.01-14.36.04.jpg'),
 	(8, 2, '../img_pro/2023.12.01-14.38.36.jpg'),
 	(9, 17, '../img_pro/2023.12.01-14.40.03.jpg'),
@@ -110,25 +115,19 @@ REPLACE INTO `imgpro` (`id_imgpro`, `id_pro`, `img`) VALUES
 	(20, 5, '../img_pro/2023.12.01-14.47.00.jpg'),
 	(21, 4, '../img_pro/2023.12.01-14.47.27.jpg'),
 	(22, 3, '../img_pro/2023.12.01-14.47.59.jpg');
-/*!40000 ALTER TABLE `imgpro` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.parceiro: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `parceiro` DISABLE KEYS */;
-REPLACE INTO `parceiro` (`id_par`, `nome`, `email`, `senha`) VALUES
+INSERT INTO `parceiro` (`id_par`, `nome`, `email`, `senha`) VALUES
 	(1, 'Yara', 'aara@gmail.com', '$2y$10$ar0u6umBz8qT0fcgMD/5Me6ZmxdYBYLHUwPmkja/zPyIbC0P2zkEe'),
 	(2, 'Yara', 'aara@gmail.com', '$2y$10$JbOc0m/SggGuhDFjHt7soe9yKHAJuaICFwhogc2qkxDZ8xiHHE0Qm');
-/*!40000 ALTER TABLE `parceiro` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.pedido: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-REPLACE INTO `pedido` (`id_ped`, `id_user`, `id_pro`, `numero`, `cor`, `tamanho`, `qtd`, `valor`) VALUES
+INSERT INTO `pedido` (`id_ped`, `id_user`, `id_pro`, `numero`, `cor`, `tamanho`, `qtd`, `valor`) VALUES
 	(1, 1, 2, 4, 'Roxo', '15cm', 30, 600.00),
 	(2, 1, 1, 4, 'Roxo', 'M', 10, 150.00);
-/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 
 -- Copiando dados para a tabela save.produto: ~16 rows (aproximadamente)
-/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-REPLACE INTO `produto` (`id_pro`, `id_cat`, `prod`, `preco`, `modelo`, `img_pro`) VALUES
+INSERT INTO `produto` (`id_pro`, `id_cat`, `prod`, `preco`, `modelo`, `img_pro`) VALUES
 	(1, 1, 'Vestido', 25.00, 'Plaid Print Pet Dress', '../../listagem/img/2023.12.01-13.39.18.jpg'),
 	(2, 1, 'Moletom', 30.00, 'Tracksuit Top', '../../listagem/img/2023.12.01-13.42.12.jpg'),
 	(3, 3, 'Sapatinho', 10.00, '5 Best Dog Boots of 2023', '../../listagem/img/2023.12.01-13.47.21.jpg'),
@@ -138,18 +137,17 @@ REPLACE INTO `produto` (`id_pro`, `id_cat`, `prod`, `preco`, `modelo`, `img_pro`
 	(7, 4, 'Cama para gatos', 130.00, 'Cama para Gatos - Pet Circle - Amarelo', '../../listagem/img/2023.12.01-14.03.51.jpg'),
 	(8, 4, 'Peitoral ', 120.00, 'Peitoral com mochila embutida', '../../listagem/img/2023.12.01-14.05.46.jpg'),
 	(9, 5, 'Racao Special Dog ', 150.00, 'Special Dog sabor Carne e legumes 15kg', '../../listagem/img/2023.12.01-14.09.02.jpg'),
-	(10, 9, 'Pote de armazenamento', 40.00, 'Pote de armazenamento de racao ', '../../listagem/img/2023.12.01-14.11.01.jpg'),
+	(10, 9, 'Pote de armazenamento', 40.00, 'Pote de armazenamento de raÃ§Ã£o ', '../../listagem/img/2023.12.01-14.11.01.jpg'),
 	(11, 8, 'Kit banho', 100.00, 'kit banho/shampoo, condicionador e perfume ', '../../listagem/img/2023.12.01-14.13.47.jpg'),
 	(12, 8, 'Kit salao', 107.00, 'kit salao, escova para pelos longos,curtos e cortador de unha', '../../listagem/img/2023.12.01-14.15.43.jpg'),
 	(13, 8, 'Tapete higienico', 50.00, 'Tapete Higienico Lavavel para Caes', '../../listagem/img/2023.12.01-14.17.22.jpg'),
 	(16, 8, 'Saco Coletor De Fezes De Cachorro Kit 19 Rolos', 40.00, 'Saco Coletor De Fezes De Cachorro Kit 19 Rolos', '../../listagem/img/2023.12.01-14.21.06.jpg'),
 	(17, 1, 'Moletom com capuz', 50.00, 'Moletom com capuz Design de dinossauros', '../../listagem/img/2023.12.01-14.26.07.jpg'),
-	(18, 1, 'Sueter de croche', 30.00, 'Sueter de croche para gatos', '../../listagem/img/2023.12.01-14.27.40.jpg');
-/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+	(18, 1, 'Sueter de croche', 30.00, 'Sueter de croche para gatos', '../../listagem/img/2023.12.01-14.27.40.jpg'),
+	(19, 1, 'Kit primavera verao', 100.00, ' Spring Summer Dog Dress Dog ', '../../listagem/img/2023.12.06-14.32.01.jpg');
 
 -- Copiando dados para a tabela save.tamanhos: ~27 rows (aproximadamente)
-/*!40000 ALTER TABLE `tamanhos` DISABLE KEYS */;
-REPLACE INTO `tamanhos` (`id_tam`, `id_pro`, `tamanho`) VALUES
+INSERT INTO `tamanhos` (`id_tam`, `id_pro`, `tamanho`) VALUES
 	(1, 1, 'P'),
 	(2, 2, 'P'),
 	(3, 2, 'G'),
@@ -176,19 +174,17 @@ REPLACE INTO `tamanhos` (`id_tam`, `id_pro`, `tamanho`) VALUES
 	(25, 17, 'G'),
 	(26, 18, 'M'),
 	(27, 18, 'P'),
-	(28, 17, 'G');
-/*!40000 ALTER TABLE `tamanhos` ENABLE KEYS */;
+	(28, 17, 'G'),
+	(29, 19, 'porte pequeno');
 
--- Copiando dados para a tabela save.user: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-REPLACE INTO `user` (`id_user`, `usuario`, `nome`, `email`, `telefone`, `senha`, `foto`, `cep`, `pais`, `estado`, `cidade`, `bairro`, `rua`, `numero`) VALUES
-	(1, 'Daiane', 'Daiane MagalhÃ£es de Castro', 'daianemagalhaesdecasro@gmail.com', '+5511961504684', '$2y$10$8SJY9ENUSrJbRhi0iSv1R.r3Pk6saaEf6CTdA2eD.RRl/.7Ppkk1S', '../img/padrao.png', '06535020', 'Brasil', 'RN', 'Santana', 'cid. SÃ£o Pedro', 'Av. Fortunato Camargo', '992');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- Copiando dados para a tabela save.user: ~1 rows (aproximadamente)
+INSERT INTO `user` (`id_user`, `usuario`, `nome`, `email`, `telefone`, `senha`, `foto`, `cep`, `pais`, `estado`, `cidade`, `bairro`, `rua`, `numero`) VALUES
+	(1, 'Daiane', 'Daiane MagalhÃ£es de Castro', 'daianemagalhaesdecasro@gmail.com', '+5511961504684', '$2y$10$8SJY9ENUSrJbRhi0iSv1R.r3Pk6saaEf6CTdA2eD.RRl/.7Ppkk1S', '../img/padrao.png', '06535020', 'Brasil', 'RN', 'Santana', 'cid. SÃ£o Pedro', 'Av. Fortunato Camargo', '992'),
+	(2, 'Miih', 'Yasmin Santos', 'yasminsantosyasxp@gmail.com', '11 913280693', '$2y$10$bH1ZnYKV9BSpbz99qH0LjexgyJCxf796jRKr4xM/JwrO5KfCdbGl6', '../img/padrao.png', '06528086', 'Brasil', 'SP', 'Santana de Parnaíba', 'Varzéa de Souza', 'Rua do Astronauta', '280');
 
 -- Copiando dados para a tabela save.voluntario: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `voluntario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `voluntario` ENABLE KEYS */;
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
